ec9dba6db159242335949b50408327cb6ad3a7bf
Lewis Larsen <lewis@larsens.dev>
2024-07-28 00:31:43 +0100

    updates

--- src/App.vue
+++ src/App.vue
@@ -181,8 +181,7 @@ onMounted(() => {
         </div>
         <div class="mt-12 border-t border-gray-800 pt-8 flex flex-col md:flex-row justify-between items-center">
           <p class="text-gray-400 text-sm">© {{ new Date().getFullYear() }} Vanguard.</p>
-          <p class="text-gray-400 text-sm mt-4 md:mt-0 font-normal">Vanguard is an open-source project licensed under the MIT
-            License.</p>
+          <p class="text-gray-400 text-sm mt-4 md:mt-0 font-normal">Vanguard is an open-source project licensed under AGPL 3.0.</p>
         </div>
       </div>
     </footer>
